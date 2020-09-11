f48d17815456735e938701b71da683a588f96c99
swamini03 <70734052+swamini03@users.noreply.github.com>
2020-09-11 13:24:34 +0530

    Add files via upload

---
+++ Day3-4/q1.js
@@ -0,0 +1,16 @@
+ function doSomething(){
+     const img=document.getElementById("picture");
+     img.src="736230.jpeg";
+ }
+
+
+function change(){
+    const img=document.getElementById("picture");
+    img.src="img2.jpg";
+    }
+
+function again(){
+    const img=document.getElementById("picture");
+    img.src="img3.jpg";
+}
+
